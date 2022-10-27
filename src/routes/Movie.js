b02f7202d67bfc4fc969d016d30adf6f2a3477fe
yougotmyphone <110523886+themeprovider@users.noreply.github.com>
2022-10-27 17:15:32 +0900

    apolloClient

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div></div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
